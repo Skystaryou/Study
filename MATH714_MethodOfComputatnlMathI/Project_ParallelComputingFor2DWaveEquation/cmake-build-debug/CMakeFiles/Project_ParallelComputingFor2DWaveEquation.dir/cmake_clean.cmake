7dae69b0c4122300df12700db8e167244e1d633a
wenxin <wenxinsjtu2016@163.com>
2020-11-06 14:17:45 +0800

    create: create project.

---
+++ MATH714_MethodOfComputatnlMathI/Project_ParallelComputingFor2DWaveEquation/cmake-build-debug/CMakeFiles/Project_ParallelComputingFor2DWaveEquation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_ParallelComputingFor2DWaveEquation.dir/main.cpp.o"
+  "Project_ParallelComputingFor2DWaveEquation"
+  "Project_ParallelComputingFor2DWaveEquation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_ParallelComputingFor2DWaveEquation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
